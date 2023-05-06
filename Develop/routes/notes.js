ee2d3fe3631ab9b04d1046b88801264607d7a325
Jeremy <jejedech.63@gmail.com>
2023-05-06 10:59:16 +1000

    adding uuid npm

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -11,6 +11,23 @@ notes.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+// DELETE Route for a specific note
+tips.delete('/:tip_id', (req, res) => {
+  const tipId = req.params.tip_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter((tip) => tip.tip_id !== tipId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/tips.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${tipId} has been deleted 🗑️`);
+    });
+});
+
 
 
 module.exports = notes;
