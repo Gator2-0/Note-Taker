9d5f4d5520e84cadfbcd0d03d3e9875f2bc9cc14
Jeremy <jejedech.63@gmail.com>
2023-05-06 10:53:22 +1000

    adding helpers

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
+
+// Import modular routers for /notes
+const notesRouter = require('./notes')
+
 const app = express();
 
-//app.use('',)
+app.use('/notes',notesRouter)
 
 module.exports = app;
